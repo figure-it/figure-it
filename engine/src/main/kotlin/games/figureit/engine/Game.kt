a3385239cb01dfbca100fb196b813a0909aab819
Smehhh <e.qein666@gmail.com>
2020-11-30 21:35:04 +0300

    add queque implementation frame

--- engine/src/main/kotlin/games/figureit/engine/Game.kt
+++ engine/src/main/kotlin/games/figureit/engine/Game.kt
@@ -4,5 +4,7 @@ interface Game {
     fun getAllPlayers(): List<Player>
     fun getCurrentFigure(): Figure
     fun getMapSize(): Size
-
+    fun move(player: Player, move: Move)
+    fun start()
+    fun stop()
 }
---
+++ engine/src/main/kotlin/games/figureit/engine/QueueImplementation.kt
@@ -0,0 +1,27 @@
+package games.figureit.engine
+
+class QueueImplementation: Game {
+    override fun getAllPlayers(): List<Player> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCurrentFigure(): Figure {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMapSize(): Size {
+        TODO("Not yet implemented")
+    }
+
+    override fun move(player: Player, move: Move) {
+        TODO("Not yet implemented")
+    }
+
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
+}
