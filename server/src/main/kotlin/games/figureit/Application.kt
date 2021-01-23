19906b4a40e39f569286b014d30f37c66c8950ee
Andrey Sundukov <andrey.sundukov@vrbank.ru>
2021-01-23 10:21:24 +0100

    implement docker image push on releases

--- server/src/main/kotlin/games/figureit/Application.kt
+++ server/src/main/kotlin/games/figureit/Application.kt
@@ -1,11 +1,9 @@
+@file:JvmName("Application")
+
 package games.figureit
 
 import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    SpringApplication.run(ServerApplication::class.java, *args)
 }
