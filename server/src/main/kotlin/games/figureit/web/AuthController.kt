654a112c3ef035679f1f1309565d2937c7cf0efc
Andrey Sundukov <andrey.sundukov@vrbank.ru>
2021-01-25 09:09:17 +0100

    delete get for authorize

--- server/src/main/kotlin/games/figureit/web/AuthController.kt
+++ server/src/main/kotlin/games/figureit/web/AuthController.kt
@@ -3,6 +3,7 @@ package games.figureit.web
 import games.figureit.service.GameService
 import games.figureit.web.model.PlayerResponse
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,7 +13,7 @@ class AuthController(
     private val gameService: GameService
 ) {
 
-    @RequestMapping(produces = [APPLICATION_JSON_VALUE])
+    @PostMapping(produces = [APPLICATION_JSON_VALUE])
     fun request(): PlayerResponse {
         val player = gameService.addPlayer()
         return PlayerResponse(player)
