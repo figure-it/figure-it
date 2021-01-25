894dc6e7a60d0eb61c9fdd1555654782a8b834cc
Andrey Sundukov <andrey.sundukov@vrbank.ru>
2021-01-25 08:28:48 +0100

    add status endpoint

--- server/src/main/kotlin/games/figureit/web/PingController.kt
+++ server/src/main/kotlin/games/figureit/web/PingController.kt
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/status")
 class PingController {
-    @RequestMapping(produces = [APPLICATION_JSON_VALUE])
-    fun request(): String {
+    @RequestMapping(value = arrayOf("/status"), produces = [APPLICATION_JSON_VALUE])
+    fun status(): String {
         return "Up"
+    }
+    @RequestMapping(value = arrayOf("/"), produces = [APPLICATION_JSON_VALUE])
+    fun hello(): String {
+        return "Figure-it"
     }
 }
