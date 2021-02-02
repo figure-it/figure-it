60ee67582007ede5d204816b416f771af97f5666
Andrey Sundukov <andrey.sundukov@vrbank.ru>
2021-02-02 08:08:50 +0100

    add CORS

--- server/src/main/kotlin/games/figureit/web/PingController.kt
+++ server/src/main/kotlin/games/figureit/web/PingController.kt
@@ -1,10 +1,12 @@
 package games.figureit.web
 
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class PingController {
     @RequestMapping(value = arrayOf("/status"), produces = [APPLICATION_JSON_VALUE])
     fun status(): String {
