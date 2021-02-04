43880be93a1655144560a9e1e9c003c8158b5193
Andrey Sundukov <andrey.sundukov@vrbank.ru>
2021-02-04 22:18:12 +0100

    add cross to authorization

--- server/src/main/kotlin/games/figureit/web/AuthController.kt
+++ server/src/main/kotlin/games/figureit/web/AuthController.kt
@@ -3,10 +3,12 @@ package games.figureit.web
 import games.figureit.service.GameService
 import games.figureit.web.model.PlayerResponse
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 @RequestMapping("/game/authorize")
 class AuthController(
